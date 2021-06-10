758d2080b53b007bf48b9ec3a9d1eb5982628483
Matheus Rebola <m.cavalcantir@outlook.com>
2021-06-10 11:13:52 -0300

    primeiro commit

---
+++ PS3SI/frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Segundo Projeto do capítulo de React.">
+        <div className='display-4'>Bem Vindos Aulunos e Professores do PS3SI!</div>
+        <hr />
+        <p className="mb-0">Sistema para cadastro desenvolvido em React!</p>
+    </Main>
